7b987e958f2277eaa6a77be691a26fa71769db61
Joe Pham <rungxanh2901@gmail.com>
2022-07-15 19:36:30 -0400

    Solve issue with localizing score string

--- [03] Guess Flag/Guess Flag/Views/GFScoreView.swift
+++ [03] Guess Flag/Guess Flag/Views/GFScoreView.swift
@@ -11,12 +11,11 @@ struct GFScoreView: View {
 	var score: Int
 	
     var body: some View {
-		/// String wrapped by `Text`, interpolated with another variable
-		/// to create a `LocalizedStringKey` for translation
-		let scoreString: LocalizedStringKey = "\(Text("Score")): \(score)"
-		
-		Text(scoreString)
-			.font(.title.bold())
-			.foregroundColor(.white)
+		HStack(spacing: 0) {
+			Text("Score")
+			Text(": \(score)")
+		}
+		.font(.title.bold())
+		.foregroundColor(.white)
     }
 }
