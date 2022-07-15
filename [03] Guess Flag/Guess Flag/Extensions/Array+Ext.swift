433ad863ae22aa5fbaca78cb85dc81dd1d9a2d15
Joe Pham <rungxanh2901@gmail.com>
2022-07-15 17:58:50 -0400

    Rename array extension file

--- [03] Guess Flag/Guess Flag/Extensions/Array+Ext.swift
+++ [03] Guess Flag/Guess Flag/Extensions/Array+Ext.swift
@@ -1,5 +1,5 @@
 //
-//  Collection+Ext.swift
+//  Array+Ext.swift
 //  Guess Flag
 //
 //  Created by Joe Pham on 2022-07-14.
@@ -7,9 +7,9 @@
 
 import Foundation
 
-extension Collection {
+extension Array {
 	
-	/// A readable extension to get an element at assigned index in the collection
+	/// A readable extension to get an element at assigned index in the array
 	func element(_ index: Index) -> Self.Element {
 		return self[index]
 	}
