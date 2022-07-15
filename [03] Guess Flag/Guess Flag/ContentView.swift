5414000026b766d7ed658b6040b02efc55205eef
Joe Pham <rungxanh2901@gmail.com>
2022-07-14 21:41:45 -0400

    Starter code for project

---
+++ [03] Guess Flag/Guess Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess Flag
+//
+//  Created by Joe Pham on 2022-07-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		Text("Hello, world!")
+			.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+		ContentView()
+    }
+}
---
+++ [03] Guess Flag/Guess Flag/Guess_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_FlagApp.swift
+//  Guess Flag
+//
+//  Created by Joe Pham on 2022-07-14.
+//
+
+import SwiftUI
+
+@main
+struct Guess_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
