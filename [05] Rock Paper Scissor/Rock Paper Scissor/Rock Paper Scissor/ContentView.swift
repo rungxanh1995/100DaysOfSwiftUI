ffcfdfa0696eec0d434ea53178b8d742f70b9c49
Joe Pham <rungxanh2901@gmail.com>
2022-07-16 21:15:37 -0400

    Starter code

---
+++ [05] Rock Paper Scissor/Rock Paper Scissor/Rock Paper Scissor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissor
+//
+//  Created by Joe Pham on 2022-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
