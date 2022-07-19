efef148b20e63c2fb75008969ec5b1600238a93c
Joe Pham <rungxanh2901@gmail.com>
2022-07-19 16:57:27 -0400

    Update default shadow modifier color

--- [05] Rock Paper Scissor/Rock Paper Scissor/Rock Paper Scissor/Modifiers/LightShadowModifier.swift
+++ [05] Rock Paper Scissor/Rock Paper Scissor/Rock Paper Scissor/Modifiers/LightShadowModifier.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LightShadowModifier: ViewModifier {
-	private(set) var color: Color = .black
+	private(set) var color: Color = Color(.sRGBLinear, white: 0, opacity: 0.33)
 	
 	func body(content: Content) -> some View {
 		return content
