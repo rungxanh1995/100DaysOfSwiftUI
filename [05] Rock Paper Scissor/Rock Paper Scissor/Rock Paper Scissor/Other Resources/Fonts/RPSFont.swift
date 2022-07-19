e38690c7086fe585ba499b3614c2f00e1b1a7230
Joe Pham <rungxanh2901@gmail.com>
2022-07-18 23:25:54 -0400

    Add custom small font

--- [05] Rock Paper Scissor/Rock Paper Scissor/Rock Paper Scissor/Other Resources/Fonts/RPSFont.swift
+++ [05] Rock Paper Scissor/Rock Paper Scissor/Rock Paper Scissor/Other Resources/Fonts/RPSFont.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 struct RPSFont {
+	static let LoraSmall = Font.custom("Lora-Regular", size: 18, relativeTo: .caption)
 	static let Lora = Font.custom("Lora-Regular", size: 24, relativeTo: .body)
 	static let LoraMedium = Font.custom("Lora-Medium", size: 24, relativeTo: .body)
 	static let LoraBold = Font.custom("Lora-Bold", size: 24, relativeTo: .body)
