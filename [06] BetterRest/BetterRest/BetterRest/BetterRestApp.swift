32cfbc031f37258bf64ae19c419375399b4413b7
Joe Pham <rungxanh2901@gmail.com>
2022-07-20 16:26:54 -0400

    Starter code

---
+++ [06] BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Joe Pham on 2022-07-20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
