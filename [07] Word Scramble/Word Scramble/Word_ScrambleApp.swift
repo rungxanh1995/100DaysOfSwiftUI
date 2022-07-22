4aeba31cb90b6b1117464ea391690b67166c4e63
Joe Pham <rungxanh2901@gmail.com>
2022-07-22 10:16:13 -0400

    Starter code

---
+++ [07] Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Joe Pham on 2022-07-22.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
