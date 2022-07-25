b0952e19358fb206764da39ea3d1c3b2ea4155ad
Joe Pham <rungxanh2901@gmail.com>
2022-07-25 11:18:49 -0400

    <again> Attempt to couple game logic with view

--- [07] Word Scramble/Word Scramble/ContentView.swift
+++ [07] Word Scramble/Word Scramble/ContentView.swift
@@ -12,9 +12,8 @@ import SwiftUI
 
 struct ContentView: View {
 	@State
-	private var usedWords = [String]()
-	@State
-	private var rootWord = ""
+	private var game = WSGame(rootWord: "")
+
 	@State
 	private var newWord = ""
 	
@@ -33,7 +32,7 @@ struct ContentView: View {
 				) {
 					HStack {
 						Spacer()
-						Text(rootWord)
+						Text(game.rootWord)
 							.font(.title)
 							.foregroundColor(.primary)
 						Spacer()
@@ -49,7 +48,7 @@ struct ContentView: View {
 				}
 				
 				Section {
-					ForEach(usedWords, id: \.self) { usedWord in
+					ForEach(game.usedWords, id: \.self) { usedWord in
 						HStack {
 							Text(usedWord)
 							Spacer()
@@ -74,15 +73,15 @@ struct ContentView: View {
     }
 	
 	private func addNewWord() {
-		let answer = newWord.cleanedAndLowercased()
+		game.userAnswer = newWord.cleanedAndLowercased()
 		
 		// validation for answer here
-		guard hasPassedValidations(answer) else {
+		guard hasPassedValidations(game.userAnswer) else {
 			return
 		}
 		
 		withAnimation {
-			usedWords.insert(answer, at: 0)
+			game.usedWords.insert(game.userAnswer, at: 0)
 		}
 		newWord = ""
 	}
@@ -93,17 +92,17 @@ struct ContentView: View {
 			return false
 		}
 		
-		guard isOriginal(word: answer) else {
+		guard game.answerIsNotAlreadyUsed() else {
 			wordError(title: "Word used already", message: "Be more original")
 			return false
 		}
 		
-		guard isPossible(word: answer) else {
-			wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+		guard game.answerHasLettersFromRootWord() else {
+			wordError(title: "Word not possible", message: "You can't spell that word from '\(game.rootWord)'!")
 			return false
 		}
 		
-		guard isReal(word: answer) else {
+		guard game.answerIsRealInEnglish() else {
 			wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
 			return false
 		}
@@ -111,46 +110,11 @@ struct ContentView: View {
 		return true
 	}
 	
-		/// Check whether the word has been used before or not
-	private func isOriginal(word: String) -> Bool {
-		!usedWords.contains(word)
-	}
-	
-	
-		/// Check whether a random word can be made out of the available letters from the root word
-		/// and remove each matching letter so it can't be used twice
-	private func isPossible(word: String) -> Bool {
-		var tempWord = rootWord
-		
-		for character in word {
-			if let matchedCharacterPosition = tempWord.firstIndex(of: character) {
-				tempWord.remove(at: matchedCharacterPosition)
-			} else {
-				return false
-			}
-		}
-		
-		return true
-	}
-	
-	private func isReal(word: String) -> Bool {
-		let spellChecker = UITextChecker()
-		let newWordRange = NSRange(location: 0, length: word.utf16.count)
-		let misspelledRange = spellChecker.rangeOfMisspelledWord(
-			in: word,
-			range: newWordRange,
-			startingAt: 0, wrap: false,
-			language: "en"
-		)
-		
-		return misspelledRange.location == NSNotFound
-	}
-	
 	func startGame() {
 		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
 			if let startWords = try? String(contentsOf: startWordsURL) {
 				let allWords = startWords.components(separatedBy: "\n")
-				rootWord = allWords.randomElement() ?? "silkworm"
+				game.rootWord = allWords.randomElement() ?? "silkworm"
 				return
 			}
 		}
