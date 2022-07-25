cc269ac72b341552d65df4e005743d653646743f
Joe Pham <rungxanh2901@gmail.com>
2022-07-25 12:34:12 -0400

    Decouple spellchecker

--- [07] Word Scramble/Word Scramble/Models/WSGame.swift
+++ [07] Word Scramble/Word Scramble/Models/WSGame.swift
@@ -5,7 +5,7 @@
 //  Created by Joe Pham on 2022-07-25.
 //
 
-import UIKit
+import Foundation
 
 struct WSGame {
 	
@@ -89,17 +89,8 @@ struct WSGame {
 	}
 	
 	private func isReal(_ word: String, in language: String) -> Bool {
-		// TODO: Refactor spellchecker to a wrapper class
-		let spellChecker = UITextChecker()
-		let newWordRange = NSRange(location: 0, length: word.utf16.count)
-		let misspelledRange = spellChecker.rangeOfMisspelledWord(
-			in: word,
-			range: newWordRange,
-			startingAt: 0, wrap: false,
-			language: language
-		)
-		
-		return misspelledRange.location == NSNotFound
+		let spellChecker = WSSpellchecker(wordToCheck: word, languageCode: language)
+		return spellChecker.wordSpelledCorrectly()
 	}
 	
 	// MARK: - Error alert
