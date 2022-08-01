9d8fe03b0e08d6af997e82e337dc614c39ea7d57
Joe Pham <rungxanh2901@gmail.com>
2022-08-01 12:26:07 -0400

    Starter code

---
+++ Multiple Hero/Multiple Hero/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Multiple Hero
+//
+//  Created by Joe Pham on 2022-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
