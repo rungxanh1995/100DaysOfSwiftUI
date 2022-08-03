f5e9cc134e3f515760e1761ea0209f66b07585a2
Joe Pham <rungxanh2901@gmail.com>
2022-08-03 10:15:30 -0400

    Starter code

---
+++ [10] iExpenses/iExpenses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpenses
+//
+//  Created by Joe Pham on 2022-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
