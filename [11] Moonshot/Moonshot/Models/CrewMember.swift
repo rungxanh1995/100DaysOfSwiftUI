4ad98c26c585f066756b83708c3cdecb98aa5069
Joe Pham <rungxanh2901@gmail.com>
2022-08-09 20:54:12 -0400

    Add documentation to struct CrewMember

--- [11] Moonshot/Moonshot/Models/CrewMember.swift
+++ [11] Moonshot/Moonshot/Models/CrewMember.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/// A designated struct to serve mapping a `Mission.Crew` type.
+/// Check out `MissionViewModel.swift` for implementation of this mapping.
+///
+/// Though it's similar to `Mission.Crew` from its look,
+/// these two structs are not similar in structure.
 struct CrewMember {
 	let role: String
 	let astronaut: Astronaut
