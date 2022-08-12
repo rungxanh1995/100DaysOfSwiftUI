c8686991bbe5d65ecf105a92a5877866dcf0af93
Joe Pham <rungxanh2901@gmail.com>
2022-08-12 17:32:17 -0400

    Add starter code

---
+++ Better You/Better You/Better_YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_YouApp.swift
+//  Better You
+//
+//  Created by Joe Pham on 2022-08-12.
+//
+
+import SwiftUI
+
+@main
+struct Better_YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better You/Better You/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Better You
+//
+//  Created by Joe Pham on 2022-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		Text("Hello, world")
+			.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
