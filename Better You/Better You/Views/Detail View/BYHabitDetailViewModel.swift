cbf4be528ec201c6fbc8c9e255e5563c43122f92
Joe Pham <rungxanh2901@gmail.com>
2022-08-15 21:37:11 -0400

    Add habit detail view model

---
+++ Better You/Better You/Views/Detail View/BYHabitDetailViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  BYHabitDetailViewModel.swift
+//  Better You
+//
+//  Created by Joe Pham on 2022-08-15.
+//
+
+import SwiftUI
+
+extension BYHabitDetailView {
+	final class ViewModel: ObservableObject {
+		let item: HabitItem
+		
+		init(item: HabitItem) {
+			self.item = item
+		}
+		
+		func colorForCompletedTimes() -> Color {
+			switch item.completedTimes {
+				case 0..<5: return Color.mint
+				case 5..<10: return Color.orange
+				case 10..<Int.max: return Color.red
+				default: return Color.primary
+			}
+		}
+		
+		func colorForLastUpdated() -> Color {
+			return Color.secondary
+		}
+	}
+}
