333fb0ae3f9bdb0a67382ac09164e8ce59accf4a
Joe Pham <rungxanh2901@gmail.com>
2022-08-20 07:22:02 -0400

    Refactor checkout view model with an api service

--- [14] Cupcake Corner/Cupcake Corner/Views/Check Out/CheckoutViewModel.swift
+++ [14] Cupcake Corner/Cupcake Corner/Views/Check Out/CheckoutViewModel.swift
@@ -12,6 +12,8 @@ extension CheckoutView {
 		@Published
 		var order: Order
 		
+		private var apiService: OrderService
+		
 		private(set) var decorativeCupcakeImage: URL?
 
 		@Published
@@ -20,52 +22,26 @@ extension CheckoutView {
 		@Published
 		private(set) var showingConfirmation: Bool
 		
-		init(order: Order) {
+		init(
+			order: Order
+		) {
 			self.order = order
+			self.apiService = OrderServiceImpl(order: order)
 			self.decorativeCupcakeImage = Constants.randomDecorativeCupcakeUrl
 			confirmationMessage = ""
 			showingConfirmation = false
 		}
 		
 		func placeOrder() async -> Void {
-			guard let encodedData = encodeOrderObject() else { return }
-			let networkRequest = createAndConfigNetworkRequest()
 			do {
-				let confirmedOrder = try await fetch(networkRequest, from: encodedData)
+				guard let confirmedOrder = try await apiService.uploadAndReceive() else { return }
 				updateConfirmationContent(from: confirmedOrder)
 			} catch {
 				updateConfirmationContentForFailure()
 			}
 		}
 		
-		/// Convert current order object into some JSON data that can be sent
-		private func encodeOrderObject() -> Data? {
-			if let encoded = try? JSONEncoder().encode(self.order) {
-				return encoded
-			}
-			
-			print("Failed to encode order")
-			return nil
-		}
-		
-		/// Set up an `URLRequest` to send data over a network call
-		private func createAndConfigNetworkRequest() -> URLRequest {
-			var request = URLRequest(url: Constants.Api.baseUrl)
-			request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-			request.httpMethod = "POST"
-
-			return request
-		}
-		
-		/// Run a pre-configured network request,
-		/// and process the response sent back from an API service
-		private func fetch(_ request: URLRequest, from inputData: Data) async throws -> Order {
-				let urlSession = URLSession.shared
-				let (dataReceivedFromApiService, _) = try await urlSession.upload(for: request, from: inputData)
-				return try JSONDecoder().decode(Order.self, from: dataReceivedFromApiService)
-		}
-		
-		private func updateConfirmationContent(from decodedOrder: Order) {
+		private func updateConfirmationContent(from decodedOrder: Order) -> Void {
 			DispatchQueue.main.async { [weak self] in
 				self?.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Cupcake.types[decodedOrder.item.type].rawValue.lowercased()) cupcakes is on its way!"
 				self?.showingConfirmation.toggle()
