06f2761d78e3feec86b93349c0382adde679a792
Joe Pham <rungxanh2901@gmail.com>
2022-08-19 21:30:26 -0400

    Add a few more cupcake image urls

--- [14] Cupcake Corner/Cupcake Corner/Other Assets/Constants.swift
+++ [14] Cupcake Corner/Cupcake Corner/Other Assets/Constants.swift
@@ -15,7 +15,9 @@ struct Constants {
 		URL(string: "https://img.freepik.com/premium-vector/delicious-cupcakes-vector-sprinkles-muffin-set-isolated-white-background-eps-10_505557-888.jpg?w=600"),
 		URL(string: "https://img.freepik.com/premium-vector/icon-set-yummy-colored-cupcakes_124848-178.jpg?w=600"),
 		URL(string: "https://img.freepik.com/premium-vector/cute-raspberry-dessert-vector-illustration_650511-572.jpg?w=600"),
-		URL(string: "https://img.freepik.com/premium-vector/cupcakes-with-swirled-rainbow-icing-tasty-shiny-muffins-with-colorful-cream_499431-714.jpg?w=600")
+		URL(string: "https://img.freepik.com/premium-vector/cupcakes-with-swirled-rainbow-icing-tasty-shiny-muffins-with-colorful-cream_499431-714.jpg?w=600"),
+		URL(string: "https://www.seekpng.com/png/detail/199-1999573_cute-cupcake-clip-art-cute-cartoon-image-halloween.png"),
+		URL(string: "https://t3.ftcdn.net/jpg/04/98/88/88/360_F_498888820_SwV5fIv8XMcAFLfZghofHrWgoj4JNMjw.jpg")
 	]
 			
 	static var randomDecorativeCupcakeUrl: URL? {
