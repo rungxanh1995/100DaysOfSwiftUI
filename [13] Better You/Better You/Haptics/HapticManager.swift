820ec23905e69b67850e4771ab68097c4370598c
Joe Pham <rungxanh2901@gmail.com>
2022-08-22 21:51:48 -0400

    Add haptic code

---
+++ [13] Better You/Better You/Haptics/HapticManager.swift
@@ -0,0 +1,24 @@
+//
+//  HapticManager.swift
+//  Better You
+//
+//  Created by Joe Pham on 2022-08-22.
+//	From this tutorial: https://www.youtube.com/watch?v=DMBcjZjK844
+//
+
+import UIKit
+
+fileprivate final class HapticManager {
+	static let shared: HapticManager = .init()
+	private let feedback: UINotificationFeedbackGenerator = .init()
+	
+	private init() { }
+	
+	func trigger(_ notifType: UINotificationFeedbackGenerator.FeedbackType) {
+		feedback.notificationOccurred(notifType)
+	}
+}
+
+func haptic(_ notifType: UINotificationFeedbackGenerator.FeedbackType) {
+	HapticManager.shared.trigger(notifType)
+}
