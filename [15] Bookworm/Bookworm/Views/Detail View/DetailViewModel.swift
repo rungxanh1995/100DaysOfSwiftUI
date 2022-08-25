0ca3495694ae9023271c47cc982a4e9ebd66b97d
Joe Pham <rungxanh2901@gmail.com>
2022-08-25 11:41:14 -0400

    Add view model of detail view

---
+++ [15] Bookworm/Bookworm/Views/Detail View/DetailViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewModel.swift
+//  Bookworm
+//
+//  Created by Joe Pham on 2022-08-25.
+//
+
+import Foundation
+
+extension DetailView {
+	
+	final class ViewModel: ObservableObject {
+		let book: Book
+		
+		/// Needed to perform a refetch in `HomeView` after adding a book
+		let parentVM: HomeView.ViewModel
+		
+		private let storageProvider: StorageProvider
+		
+		init(
+			book: Book,
+			parentVM: HomeView.ViewModel,
+			storageProvider: StorageProvider = StorageProviderImpl.standard
+		) {
+			self.book = book
+			self.parentVM = parentVM
+			self.storageProvider = storageProvider
+		}
+		
+		func deleteBook() -> Void {
+			storageProvider.context.delete(book)
+			saveThenRefetchData()
+		}
+		
+		private func saveThenRefetchData() -> Void {
+			storageProvider.saveAndHandleError()
+			parentVM.fetchBooks()
+		}
+	}
+}
