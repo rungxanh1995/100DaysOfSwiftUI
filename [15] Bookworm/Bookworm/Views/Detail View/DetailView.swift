fd3101c1a4865ec2617127bd89dcbdd1e2c62e97
Joe Pham <rungxanh2901@gmail.com>
2022-08-25 14:51:14 -0400

    Refactor watermark into separate view

--- [15] Bookworm/Bookworm/Views/Detail View/DetailView.swift
+++ [15] Bookworm/Bookworm/Views/Detail View/DetailView.swift
@@ -26,14 +26,9 @@ struct DetailView: View {
 					.resizable()
 					.scaledToFit()
 				
-				Text(viewModel.book.genre?.uppercased() ?? "FANTASY")
-					.font(.caption)
-					.fontWeight(.black)
-					.padding(8)
-					.foregroundColor(.white)
-					.background(.black.opacity(0.75))
-					.clipShape(Capsule())
-					.offset(x: -5, y: -5)
+				ImageWatermark(
+					text: Text(viewModel.book.genre?.uppercased() ?? "FANTASY")
+				)
 			}
 			
 			Text(viewModel.book.author ?? "Unknown author")
---
+++ [15] Bookworm/Bookworm/Views/Detail View/ImageWatermark.swift
@@ -0,0 +1,29 @@
+//
+//  ImageWatermark.swift
+//  Bookworm
+//
+//  Created by Joe Pham on 2022-08-25.
+//
+
+import SwiftUI
+
+struct ImageWatermark: View {
+	let text: Text
+	
+	var body: some View {
+		text
+			.font(.caption)
+			.fontWeight(.black)
+			.padding(10)
+			.foregroundColor(.white)
+			.background(.black.opacity(0.75))
+			.cornerRadius(5)
+			.offset(x: -5, y: -5)
+	}
+}
+
+struct ImageWatermark_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageWatermark(text: Text("Watermark"))
+    }
+}
