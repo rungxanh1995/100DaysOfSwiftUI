ec4126161f481495ace9f15a1b52c631486fb22b
Joe Pham <rungxanh2901@gmail.com>
2022-08-26 11:00:52 -0400

    Add starter code

---
+++ [16] Technical CoreData/Technical CoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Technical CoreData
+//
+//  Created by Joe Pham on 2022-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ [16] Technical CoreData/Technical CoreData/Technical_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Technical_CoreDataApp.swift
+//  Technical CoreData
+//
+//  Created by Joe Pham on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct Technical_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
