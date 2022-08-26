69d82d87a045ac8d79c17498d57f2b46ce2c44bb
Joe Pham <rungxanh2901@gmail.com>
2022-08-25 22:14:02 -0400

    <prepare for edit> Add input variables + placeholder update func

--- [15] Bookworm/Bookworm/Views/Detail View/DetailViewModel.swift
+++ [15] Bookworm/Bookworm/Views/Detail View/DetailViewModel.swift
@@ -10,13 +10,20 @@ import Foundation
 extension DetailView {
 	
 	final class ViewModel: ObservableObject {
-		let book: Book
+		var book: Book
 		var bookContainsReview: Bool {
 			!(book.review?.isEmpty ?? true)
 		}
 		var bookAddedDate: String? {
 			book.addedDate?.formatted(date: .abbreviated, time: .shortened)
 		}
+		
+		// MARK: - Edit Mode
+		@Published var newBookTitle: String = ""
+		@Published var newBookAuthor: String = ""
+		@Published var newBookRating: Int = 3
+		@Published var newBookGenre: String = "Programming"
+		@Published var newBookReview: String = ""
 		
 		/// Needed to perform a refetch in `HomeView` after adding a book
 		let parentVM: HomeView.ViewModel
@@ -31,6 +38,10 @@ extension DetailView {
 			self.book = book
 			self.parentVM = parentVM
 			self.storageProvider = storageProvider
+		}
+		
+		func updateBook() -> Void {
+			
 		}
 		
 		func deleteBook() -> Void {
