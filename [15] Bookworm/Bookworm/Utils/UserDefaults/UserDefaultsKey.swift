9fddc5cf5ee669757b0b792e0d4a6031ea855110
Joe Pham <rungxanh2901@gmail.com>
2022-08-26 09:34:10 -0400

    Add code for haptics & user defaults

---
+++ [15] Bookworm/Bookworm/Utils/UserDefaults/UserDefaultsKey.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsKey.swift
+//  Bookworm
+//
+//  Created by Joe Pham on 2022-08-26.
+//
+
+import Foundation
+
+enum UserDefaultsKey {
+	static let hapticsEnabled: String = "hapticsEnabled"
+	static let systemTheme: String = "systemTheme"
+}
