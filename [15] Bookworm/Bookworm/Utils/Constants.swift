dfaab04cb658f64e7541a0ca5b4e30f27a9f763f
Joe Pham <rungxanh2901@gmail.com>
2022-08-26 08:29:05 -0400

    Refactor list of book genres into constant enum

---
+++ [15] Bookworm/Bookworm/Utils/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Bookworm
+//
+//  Created by Joe Pham on 2022-08-26.
+//
+
+import Foundation
+
+enum Constants {
+	static let bookGenresSorted: [String] = [
+		"Programming",
+		"Business",
+		"Self-help",
+		"Fantasy",
+		"Horror",
+		"Kids",
+		"Mystery",
+		"Poetry",
+		"Romance",
+		"Thriller",
+		"Other"
+	].sorted()
+}
