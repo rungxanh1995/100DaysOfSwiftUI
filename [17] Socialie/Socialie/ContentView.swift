9aa8484a0b064951695d296ccc01a99ac709f2fd
Joe Pham <rungxanh2901@gmail.com>
2022-08-31 10:22:34 -0400

    Add starter code

---
+++ [17] Socialie/Socialie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Socialie
+//
+//  Created by Joe Pham on 2022-08-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ [17] Socialie/Socialie/SocialieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialieApp.swift
+//  Socialie
+//
+//  Created by Joe Pham on 2022-08-31.
+//
+
+import SwiftUI
+
+@main
+struct SocialieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
