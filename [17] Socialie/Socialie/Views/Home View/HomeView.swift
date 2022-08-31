83d606da7609072d76fa2a2a79a3145f9614f5c8
Joe Pham <rungxanh2901@gmail.com>
2022-08-31 17:35:07 -0400

    Replace homeview layout switcher with a picker

--- [17] Socialie/Socialie/Views/Home View/HomeView.swift
+++ [17] Socialie/Socialie/Views/Home View/HomeView.swift
@@ -9,30 +9,36 @@ import SwiftUI
 
 struct HomeView: View {
 	
+	/// Helps selecting the layout of `HomeView`
+	private enum Layout {
+		case asGrid
+		case asList
+	}
+	
 	@StateObject
 	private var vm: Self.ViewModel
 	
+	@State
+	private var selectedLayout: Layout
+	
 	init(vm: Self.ViewModel = .init()) {
 		_vm = StateObject(wrappedValue: vm)
+		selectedLayout = .asGrid
 	}
-	
-	@State
-	private var isGridLayout: Bool = true
 	
     var body: some View {
 		NavigationView {
 			Group {
-				if isGridLayout {
-					HomeGridView(viewModel: vm)
-				} else {
-					HomeListView(viewModel: vm)
+				switch selectedLayout {
+					case .asGrid: HomeGridView(viewModel: vm)
+					case .asList: HomeListView(viewModel: vm)
 				}
 			}
 			.task {
 				await vm.fetchData()
 			}
 			.toolbar {
-				layoutSwitchButton
+				layoutSwitcher
 			}
 		}
     }
@@ -40,15 +46,21 @@ struct HomeView: View {
 
 private extension HomeView {
 	@ViewBuilder
-	var layoutSwitchButton: some View {
-		Button {
-			isGridLayout.toggle()
-		} label: {
+	var layoutSwitcher: some View {
+		Picker("Choose layout", selection: $selectedLayout) {
 			Label(
-				"Layout",
-				systemImage: isGridLayout ? "list.bullet" : "square.grid.2x2.fill"
+				"As Grid",
+				systemImage: "square.grid.2x2.fill"
 			)
+			.tag(Layout.asGrid)
+			
+			Label(
+				"As List",
+				systemImage: "list.bullet"
+			)
+			.tag(Layout.asList)
 		}
+		.pickerStyle(.segmented)
 	}
 }
 
