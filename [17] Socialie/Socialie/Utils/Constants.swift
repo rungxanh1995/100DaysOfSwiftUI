4733b6cc26c752381dc013a46371623ea348b239
Joe Pham <rungxanh2901@gmail.com>
2022-08-31 10:31:59 -0400

    Add enum for constants

---
+++ [17] Socialie/Socialie/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Socialie
+//
+//  Created by Joe Pham on 2022-08-31.
+//
+
+import Foundation
+
+enum Constants {
+	internal enum Url {
+		/// API URL string of source JSON to fetch
+		static let friendfaceURLString: String = "https://www.hackingwithswift.com/samples/friendface.json"
+	}
+}
