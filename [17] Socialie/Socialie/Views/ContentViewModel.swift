180d5a52cb20843451ec24475d2c1e111f6bf61c
Joe Pham <rungxanh2901@gmail.com>
2022-08-31 11:04:13 -0400

    Add content view model

---
+++ [17] Socialie/Socialie/Views/ContentViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ContentViewModel.swift
+//  Socialie
+//
+//  Created by Joe Pham on 2022-08-31.
+//
+
+import Foundation
+
+extension ContentView {
+	/// View model specialized for `ContentView`
+	final class ViewModel: ObservableObject {
+		@Published
+		var users: [User]
+		
+		private let apiService: ApiService
+		
+		init(apiService: ApiService = SocialieApiService()) {
+			self.apiService = apiService
+			users = [User]()
+		}
+		
+		@MainActor
+		func fetchData() async -> Void {
+			users = await apiService.fetchData()
+		}
+	}
+}
