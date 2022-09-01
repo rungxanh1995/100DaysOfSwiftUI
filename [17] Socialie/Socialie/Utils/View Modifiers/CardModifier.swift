b4220eb62e32f7d03b5a53eaf522726d13a557f6
Joe Pham <rungxanh2901@gmail.com>
2022-09-01 19:53:12 -0400

    Add gradient to card view

--- [17] Socialie/Socialie/Utils/View Modifiers/CardModifier.swift
+++ [17] Socialie/Socialie/Utils/View Modifiers/CardModifier.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct CardModifier: ViewModifier {
+	@Environment(\.colorScheme)
+	fileprivate var colorScheme
+	
+	fileprivate var gradientColorOne: Color {
+		switch colorScheme {
+			case .light:
+				return .accentColor.opacity(0.025)
+			case .dark:
+				return .accentColor.opacity(0.2)
+			@unknown default:
+				return .clear
+		}
+	}
+	
+	fileprivate var gradientColorTwo: Color {
+		switch colorScheme {
+			case .light:
+				return .white.opacity(0.1)
+			case .dark:
+				return .clear
+			@unknown default:
+				return .clear
+		}
+	}
+	
 	func body(content: Content) -> some View {
-		content
+		ZStack {
+			LinearGradient(
+				colors: [gradientColorOne, gradientColorTwo, gradientColorOne],
+				startPoint: .topLeading,
+				endPoint: .bottomTrailing
+			)
+			content
+		}
 			.frame(maxWidth: .infinity)
 			.cornerRadius(10)
 			.overlay(
