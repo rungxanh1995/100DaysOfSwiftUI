b133bf50cd2bdf51a4c6150218f6d78f63b549a7
Joe Pham <rungxanh2901@gmail.com>
2022-08-31 20:05:51 -0400

    Revamp user view

--- [17] Socialie/Socialie/Views/User View/UserView.swift
+++ [17] Socialie/Socialie/Views/User View/UserView.swift
@@ -11,8 +11,94 @@ struct UserView: View {
 	private(set) var user: User
 	
     var body: some View {
-		Text(user.name)
+		ScrollView {
+			mainInfoCard
+			contactCard
+			aboutCard
+			
+			// TODO: Add view for tags
+			// TODO: Add view for friend list
+		}
+		.navigationTitle(user.name)
+		.navigationBarTitleDisplayMode(.inline)
     }
+}
+
+private extension UserView {
+	// MARK: - Main user info card
+	@ViewBuilder
+	var mainInfoCard: some View {
+		VStack(alignment: .leading) {
+			// Main content
+			HStack(spacing: 20) {
+				HomeCellImage(user: user)
+				VStack(alignment: .leading) {
+					// Name
+					Text(user.name)
+						.font(.title3.bold())
+						.foregroundColor(Color.accentColor)
+					// Age and Active status
+					HStack(spacing: 4) {
+						Text("\(user.age) years old")
+							.font(.callout)
+						Text("•")
+						Text(user.isActive ? "Online" : "Offline")
+							.font(.callout)
+							.foregroundColor(user.isActive ? .green : .gray)
+					}
+				}
+			}
+			
+			Divider()
+			
+			// Footer
+			VStack {
+				Text("Joined \(user.formattedRegisterDate)")
+					.font(.caption)
+					.foregroundColor(.secondary)
+			}
+			.frame(maxWidth: .infinity)
+			
+		}
+		.expandToInfinity()
+		.padding()
+		.asCard()
+	}
+	
+	// MARK: - Contact card
+	@ViewBuilder
+	var contactCard: some View {
+		VStack(alignment: .leading) {
+			Text("Contact")
+				.font(.title2.bold())
+				.foregroundColor(Color.accentColor)
+			Divider()
+			Text("Working at \(user.company)")
+			HStack(alignment: .top) {
+				Text("Email:")
+				Link(user.email, destination: URL(string: "mailto:\(user.email)")!)
+			}
+			Text("Address: \(user.address)")
+		}
+		.expandToInfinity()
+		.padding()
+		.asCard()
+	}
+	
+	// MARK: - About card
+	@ViewBuilder
+	var aboutCard: some View {
+		VStack(alignment: .leading) {
+			Text("About")
+				.font(.title2.bold())
+				.foregroundColor(Color.accentColor)
+			Divider()
+			Text(user.about)
+		}
+		.expandToInfinity()
+		.padding()
+		.asCard()
+	}
 }
 
 struct UserView_Previews: PreviewProvider {
