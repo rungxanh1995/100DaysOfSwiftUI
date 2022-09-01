1ffcfe4073b7c6508c4b46aaf93b0c3a0b6cf621
Joe Pham <rungxanh2901@gmail.com>
2022-08-31 21:10:56 -0400

    Fix typo in email symbol systemname

--- [17] Socialie/Socialie/Utils/Symbols.swift
+++ [17] Socialie/Socialie/Utils/Symbols.swift
@@ -12,7 +12,7 @@ enum Symbols {
 	enum Images {
 		static let person: Image = .init(systemName: "person.crop.circle.fill")
 		static let building: Image = .init(systemName: "building.2")
-		static let email: Image = .init(systemName: "envelop")
+		static let email: Image = .init(systemName: "envelope")
 		static let map: Image = .init(systemName: "map")
 		static let dotCircle: Image = .init(systemName:"circle.fill")
 	}
